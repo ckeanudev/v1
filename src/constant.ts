e89f506ae1ebb34d7c3161dba6b0bda01af5ed69
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2024-01-19 07:35:07 +0800

    Added constant data

---
+++ src/constant.ts
@@ -0,0 +1,242 @@
+export const myData = {
+  fullname: "Ckeanu Richer Locson",
+  links: {
+    github: "https://github.com/ckeanudev",
+    linkedin: "https://www.linkedin.com/in/ckeanuuu/",
+    facebook: "https://www.facebook.com/ckeanuuu/",
+    instagram: "https://www.instagram.com/ckeanuuu/",
+    steam: "https://steamcommunity.com/profiles/76561199565970232/",
+  },
+};
+
+export const educationalAttainment = [
+  {
+    title: "Bachelor of Science in Computer Science",
+    school: "Western Mindanao State University",
+    schoolAddress: "Normal Road, Baliwasan, Zamboanga City, Philippines",
+    awards: ["With a GWA of 1.83", "Service Award", "SE Lead at GDSC Crimsons"],
+    year: "2019 - 2023",
+  },
+  {
+    title: "Information and Communication Technology",
+    school: "Southern City Colleges",
+    schoolAddress: "Pilar Street, Zamboanga City, Philippines",
+    awards: ["With Honor", "Proficiency in Mathematics"],
+    year: "2017 - 2019",
+  },
+];
+
+export const hobbies = [
+  "👨🏻‍💻 Coding",
+  "🎧 Listening to music",
+  "🎨 Designing",
+  "🎮 Video Games",
+  "📚 Learning",
+  "🐕 Animal Training",
+  "🎥 Watching Movies",
+  "✍ Drawing",
+  "💪🏻 Exercising",
+];
+
+export const experiences = [
+  {
+    job: "Front End Developer",
+    company: "AKA Software Inc.",
+    companyAddress:
+      "JLC Building, Don Alfaro Street, Tetuan Zamboanga City, Philippines, 7000, Zamboanga City, Philippines",
+    descripion:
+      "We deliver innovative, customized, and user-friendly website solutions that align perfectly with our clients goals and objectives while specializing in the creation and development of websites for businesses across various industries.",
+
+    timeline: "Sept 2023 - Now",
+    useTools: [
+      "React",
+      "Tailwind CSS",
+      "SCSS",
+      "TypeScript",
+      "AG Grid",
+      "Postman",
+      "Prime React",
+      "Redux",
+      "Visual Studio Code",
+    ],
+    startDate: "2023-09-04",
+  },
+];
+
+export const projects = {
+  featuredProjects: [
+    {
+      title: "Showjects",
+      description:
+        "A platform where you can showcase your personal software development projects where other developers can see, comment, review your source code and give feedback about your projects to help you get better.",
+      img: `/images/FeaturedProjects/showjects.png`,
+      githubLink: "https://github.com/ckeanudev/showjects",
+      liveLink: "https://showjects.vercel.app/",
+      tools: [
+        "Next",
+        "TypeScript",
+        "Tailwind CSS",
+        "shadcn/ui",
+        "Clerk",
+        "MongoDB",
+        "Zod",
+        "Uploadthing",
+      ],
+      progress: "Coming soon",
+    },
+    {
+      title: "Showjects",
+      description:
+        "A platform where you can showcase your personal software development projects where other developers can see, comment, review your source code and give feedback about your projects to help you get better.",
+      img: `/images/FeaturedProjects/showjects.png`,
+      githubLink: "https://github.com/ckeanudev/showjects",
+      liveLink: "https://showjects.vercel.app/",
+      tools: [
+        "Next",
+        "TypeScript",
+        "Tailwind CSS",
+        "shadcn/ui",
+        "Clerk",
+        "MongoDB",
+        "Zod",
+        "Uploadthing",
+      ],
+      progress: "Done",
+    },
+    {
+      title: "AniBrary",
+      description:
+        "A website containing collections of anime provided by Jikan API an open-source PHP & REST API for the most active online anime + manga community and database. This website has all the information of your favorite anime and characters.",
+      img: `/images/FeaturedProjects/aniBrary.png`,
+      githubLink: "https://github.com/ckeanudev/anibrary",
+      liveLink: "https://anibrary.vercel.app/",
+      tools: [
+        "Next",
+        "TypeScript",
+        "Tailwind CSS",
+        "shadcn/ui",
+        "React Query",
+        "API",
+      ],
+      progress: "Done",
+    },
+  ],
+  someProjects: [
+    {
+      name: "Easybank Landing Page",
+      img: `/images/SomeProjects/sp14.png`,
+      repoLink: "https://github.com/ckeanudev/frontendmentor-easybank",
+      liveLink: "https://frontendmentor-easybank-six.vercel.app/",
+      tools: ["Astro", "React", "Tailwind CSS"],
+      featured: true,
+    },
+    {
+      name: "Ecommerce Product",
+      img: `/images/SomeProjects/sp13.png`,
+      repoLink:
+        "https://github.com/ckeanudev/frontend-mentor-ecommerce-product",
+      liveLink: "https://frontend-mentor-ecommerce-product.vercel.app/",
+      tools: ["React"],
+      featured: false,
+    },
+    {
+      name: "Results Summary Component",
+      img: `/images/SomeProjects/sp12.png`,
+      repoLink:
+        "https://github.com/ckeanudev/frontend-mentor-results-summary-component",
+      liveLink:
+        "https://frontend-mentor-results-summary-component-two.vercel.app/",
+      tools: ["HTML", "CSS"],
+      featured: false,
+    },
+    {
+      name: "Rock, Paper, Scissors Game",
+      img: `/images/SomeProjects/sp11.png`,
+      repoLink:
+        "https://github.com/ckeanudev/frontend-mentor-rock-paper-scissors-game",
+      liveLink: "https://ckeanu-frontend-mentor-game.netlify.app/",
+      tools: ["React", "API"],
+      featured: true,
+    },
+    {
+      name: "REST Countries API",
+      img: `/images/SomeProjects/sp10.png`,
+      repoLink: "https://github.com/ckeanudev/dev-rest-country-api",
+      liveLink: "https://ckeanu-country-api.netlify.app/",
+      tools: ["React", "API"],
+      featured: false,
+    },
+    {
+      name: "Job Listings w/ Filtering",
+      img: `/images/SomeProjects/sp9.png`,
+      repoLink: "https://github.com/ckeanudev/frontend-mentor-job-listing",
+      liveLink: "https://ckeanu-job-listing.netlify.app/",
+      tools: ["React"],
+      featured: false,
+    },
+    {
+      name: "Free Games Site",
+      img: `/images/SomeProjects/sp8.png`,
+      repoLink: "https://github.com/ckeanudev/dev-free-games-list",
+      liveLink: "https://ckeanu-free-games.netlify.app/",
+      tools: ["React", "API"],
+      featured: true,
+    },
+    {
+      name: "Small Meal App",
+      img: `/images/SomeProjects/sp7.png`,
+      repoLink: "https://github.com/ckeanudev/dev-react-meal-app",
+      liveLink: "https://ckeanu-meal-app.netlify.app/",
+      tools: ["React", "API"],
+      featured: false,
+    },
+    {
+      name: "COVID-19 Case Tracker App",
+      img: `/images/SomeProjects/sp6.png`,
+      repoLink: "https://github.com/ckeanudev/dev-covid-19-tracker-system",
+      liveLink: "https://ckeanu-covid-19-tracker-system.netlify.app/",
+      tools: ["React", "API"],
+      featured: true,
+    },
+    {
+      name: "Cryptocurrency Price Tracker App",
+      img: `/images/SomeProjects/sp5.png`,
+      repoLink: "https://github.com/ckeanudev/dev-crypto-price-tracker",
+      liveLink: "https://ckeanu-crypto-price-tracker.netlify.app/",
+      tools: ["React", "API"],
+      featured: false,
+    },
+    {
+      name: "Huddle Landing Page",
+      img: `/images/SomeProjects/sp4.png`,
+      repoLink: "https://github.com/ckeanudev/Huddle-landing-page",
+      liveLink: "https://huddle-landing-page-ckeanu.netlify.app/",
+      tools: ["HTML", "CSS"],
+      featured: false,
+    },
+    {
+      name: "Project Tracking Intro Component",
+      img: `/images/SomeProjects/sp3.png`,
+      repoLink: "https://github.com/ckeanudev/Project-tracking-intro-component",
+      liveLink: "https://project-tracking-intro-component-ckeanu.netlify.app/",
+      tools: ["HTML", "CSS"],
+      featured: false,
+    },
+    {
+      name: "NFT Preview Card Component",
+      img: `/images/SomeProjects/sp2.png`,
+      repoLink: "https://github.com/ckeanudev/NFT-preview-card-component",
+      liveLink: "https://nft-preview-card-component-ckeanu.netlify.app/",
+      tools: ["HTML", "CSS"],
+      featured: false,
+    },
+    {
+      name: "Order Summary Card",
+      img: `/images/SomeProjects/sp1.png`,
+      repoLink: "https://github.com/ckeanudev/Order-summary-card",
+      liveLink: "https://order-summary-card-ckeanu.netlify.app/",
+      tools: ["HTML", "CSS"],
+      featured: false,
+    },
+  ],
+};
