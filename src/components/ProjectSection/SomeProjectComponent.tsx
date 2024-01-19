1057569874a2b620e60342f8ce6b9bf2f5e8bdb6
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2024-01-20 00:23:45 +0800

    Update some project section and components

--- src/components/ProjectSection/SomeProjectComponent.tsx
+++ src/components/ProjectSection/SomeProjectComponent.tsx
@@ -1,5 +1,44 @@
-const SomeProjectComponent = ({}: any) => {
-  return <div>SomeProjectComponent</div>;
+import Image from "next/image";
+import { FaEye, FaGithub } from "react-icons/fa6";
+
+const SomeProjectComponent = ({ project }: any) => {
+  return (
+    <div className="h-full flex flex-col">
+      <div className="w-full relative">
+        <Image
+          src={project.img}
+          width={300}
+          height={150}
+          alt={project.name}
+          className="w-full h-auto rounded-lg border"
+        />
+
+        <div className="flex items-center gap-2 absolute top-3 right-3 text-dark-2">
+          <a
+            href={project.liveLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="p-1.5 border bg-light-1 rounded-full">
+            <FaEye size={16} />
+          </a>
+
+          <a
+            href={project.repoLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="p-1.5 border bg-light-1 rounded-full">
+            <FaGithub size={16} />
+          </a>
+        </div>
+      </div>
+
+      <div className="pt-2 px-1">
+        <h4 className="font-semibold text-dark-2 line-clamp-2">
+          {project.name}
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default SomeProjectComponent;
---
+++ src/components/ProjectSection/SomeProjectSection.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useState } from "react";
+import SomeProjectComponent from "./SomeProjectComponent";
+
+const SomeProjectSection = ({ someProjects }: any) => {
+  const [projectCount, setProjectCount] = useState(8);
+
+  return (
+    <>
+      <div className="w-full mt-14 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mg:gap-6">
+        {someProjects.map((project: any, i: number) => {
+          return projectCount > i ? (
+            <SomeProjectComponent project={project} key={i} />
+          ) : null;
+        })}
+      </div>
+
+      <div className="w-full flex justify-center mt-14 font-medium">
+        <button
+          className="text-dark-4"
+          onClick={() => {
+            if (projectCount === 8) {
+              setProjectCount(someProjects.length);
+            } else {
+              setProjectCount(8);
+            }
+          }}>
+          {projectCount === 8 ? `See more` : `See less`}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default SomeProjectSection;
